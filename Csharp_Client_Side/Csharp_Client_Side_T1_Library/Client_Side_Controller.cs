e32935c0771f4ba92d2a2b3410354efe628104bf
jimmi280586 <jimmiandersen9@gmail.com>
2016-11-22 11:00:08 +0100

    t2-3 added new project

---
+++ Csharp_Client_Side/Csharp_Client_Side_T1_Library/Client_Side_Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Client_Side_T1_Library
+{
+    public class Class1
+    {
+    }
+}
