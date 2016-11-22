d0e208a64ad39e48567dd73761956b442f7e0662
jimmi280586 <jimmiandersen9@gmail.com>
2016-11-22 11:34:06 +0100

    wsdl created

---
+++ Slaughter_house/src/java_t2/service/skeleton/IServerSaughterHouseSkeleton.java
@@ -0,0 +1,128 @@
+/**
+ * IServerSaughterHouseSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.3  Built on : May 30, 2016 (04:08:57 BST)
+ */
+package java_t2.service.skeleton;
+
+/**
+ *  IServerSaughterHouseSkeleton java skeleton for the axisService
+ */
+public class IServerSaughterHouseSkeleton
+    implements IServerSaughterHouseSkeletonInterface {
+    /**
+     * Auto generated method signature
+     *
+     * @param recallPack0
+     * @return recallPackResponse1
+     */
+    public java_t2.service.RecallPackResponse recallPack(
+        java_t2.service.RecallPack recallPack0) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#recallPack");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param addAnimal2
+     * @return
+     */
+    public void addAnimal(java_t2.service.AddAnimal addAnimal2) {
+        //TODO : fill this with the necessary business logic
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param addToTray3
+     * @return
+     */
+    public void addToTray(java_t2.service.AddToTray addToTray3) {
+        //TODO : fill this with the necessary business logic
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param getAnimal4
+     * @return getAnimalResponse5
+     */
+    public java_t2.service.GetAnimalResponse getAnimal(
+        java_t2.service.GetAnimal getAnimal4) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#getAnimal");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param addTray6
+     * @return
+     */
+    public void addTray(java_t2.service.AddTray addTray6) {
+        //TODO : fill this with the necessary business logic
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param addPart7
+     * @return
+     */
+    public void addPart(java_t2.service.AddPart addPart7) {
+        //TODO : fill this with the necessary business logic
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param getPart8
+     * @return getPartResponse9
+     */
+    public java_t2.service.GetPartResponse getPart(
+        java_t2.service.GetPart getPart8) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#getPart");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param getTray10
+     * @return getTrayResponse11
+     */
+    public java_t2.service.GetTrayResponse getTray(
+        java_t2.service.GetTray getTray10) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#getTray");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param addOrder12
+     * @return
+     */
+    public void addOrder(java_t2.service.AddOrder addOrder12) {
+        //TODO : fill this with the necessary business logic
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param makeOrder13
+     * @return makeOrderResponse14
+     */
+    public java_t2.service.MakeOrderResponse makeOrder(
+        java_t2.service.MakeOrder makeOrder13) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#makeOrder");
+    }
+}
